6a02e5814a4cc585754a1565d63d1d740d8054d6
Simon Fred Lubambo <slubambo57@gmail.com>
2025-06-28 16:24:09 +0300

    🚧 Wrap application in React.StrictMode for improved development checks and measure performance

--- cgpa-ui/src/index.js
+++ cgpa-ui/src/index.js
@@ -1,13 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
+
+// Optional: measure performance
+reportWebVitals();
