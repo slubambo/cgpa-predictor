1e47db574aba7e4ae3e931d289193d4c2b5b6237
Simon Fred Lubambo <slubambo57@gmail.com>
2025-06-28 16:28:12 +0300

    🚧 Add PredictionForm component for CGPA prediction input

---
+++ cgpa-ui/src/components/PredictionForm.jsx
@@ -0,0 +1,26 @@
+// src/components/PredictionForm.jsx
+import React from 'react';
+import { Box, Typography, Paper } from '@mui/material';
+import DemographicsForm from './DemographicsForm';
+
+const PredictionForm = () => {
+  return (
+    <Box sx={{ mt: 4, mb: 6 }}>
+      <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+        <Typography variant="h5" gutterBottom>
+          🎓 CGPA Prediction Form
+        </Typography>
+
+        {/* Step 1: Demographic Info */}
+        <DemographicsForm />
+
+        {/* Steps 2–4: Other forms will be added below */}
+        {/* <OLevelForm /> */}
+        {/* <ALevelForm /> */}
+        {/* <InstitutionalForm /> */}
+      </Paper>
+    </Box>
+  );
+};
+
+export default PredictionForm;
